51ea6569894b93680cac67357e25985a45e85076
Md Suny Shaikh <mdsunyshaikh@gmail.com>
2024-08-27 22:08:54 +0600

    fetch user api again

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,4 +1,10 @@
 import axios from "axios"
+import Link from "next/link"
+
+export const metadata = {
+  title: "Users",
+  description: "All users details of star-backend",
+}
 
 const UserPage = async () => {
   const { data } = await axios("http://localhost:3000/api/users")
@@ -7,13 +13,14 @@ const UserPage = async () => {
   return (
     <div>
       <h1 className="text-5xl font-bold">Users</h1>
-      <div className="mt-10">
+      <div className="mt-10 flex flex-col gap-2">
         {users.map((user) => (
-          <ul key={user.id}>
-            <li className="mt-2">
-              {user.firstName} {user.lastName}
-            </li>
-          </ul>
+          <Link
+            href={user.username}
+            key={user.id}
+          >
+            {user.firstName} {user.lastName}
+          </Link>
         ))}
       </div>
     </div>
