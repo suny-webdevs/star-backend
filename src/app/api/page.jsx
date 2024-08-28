14076207edf3d752bdc8b56031dd6e217843e51b
Md Suny Shaikh <mdsunyshaikh@gmail.com>
2024-08-28 14:13:11 +0600

    bug fix, about page

--- src/app/api/page.jsx
+++ src/app/api/page.jsx
@@ -1,19 +1,19 @@
-import Link from "next/link"
+import Link from "next/link";
 
 const Page = () => {
   return (
     <div>
-      <h3 className="text-4xl text-white font-bold">Api urls</h3>
+      <h3 className="text-4xl font-bold text-white">Api urls</h3>
       <div className="mt-5">
         <Link
           href={`${process.env.NEXT_PUBLIC_API_URL}/api/users`}
-          className="text-lg text-white font-medium"
+          className="text-lg font-medium text-white"
         >
           1. Users
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
